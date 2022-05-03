4a8abd5067edfa6cf7fd6569860de3a51a2ab154
Galoev <galoyev.ilkin@gmail.com>
2022-05-04 02:04:43 +0300

    fix obstacles

--- Assets/Scripts/NetworkManagerPong.cs
+++ Assets/Scripts/NetworkManagerPong.cs
@@ -20,16 +20,54 @@ namespace Mirror.AirHockey2077
         GameObject ball;
         List<GameObject> obstacles;
         private int NUMBER_OF_OBSTACLES = 4;
+        private List<List<Vector2>> paths;
+
+        public override void Awake()
+        {
+            base.Awake();
+
+            paths = new List<List<Vector2>>();
+
+            List<Vector2> squarePath = new List<Vector2>();
+            squarePath.Add(new Vector2(-6, 4));
+            squarePath.Add(new Vector2(-6, -4));
+            squarePath.Add(new Vector2(6, -4));
+            squarePath.Add(new Vector2(6, 4));
+            paths.Add(squarePath);
+
+            List<Vector2> trianglePath = new List<Vector2>();
+            trianglePath.Add(new Vector2(0, 3));
+            trianglePath.Add(new Vector2(-4, -3));
+            trianglePath.Add(new Vector2(4, -3));
+            paths.Add(trianglePath);
+
+            List<Vector2> horizontallPath = new List<Vector2>();
+            horizontallPath.Add(new Vector2(-6, 0));
+            horizontallPath.Add(new Vector2(6, 0));
+            paths.Add(horizontallPath);
+
+            List<Vector2> staticPath = new List<Vector2>();
+            staticPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            paths.Add(staticPath);
+
+            List<Vector2> randomPath = new List<Vector2>();
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            paths.Add(randomPath);
+        }
 
         public override void OnServerAddPlayer(NetworkConnectionToClient conn)
-        {
+        {           
             // add player at correct spawn position
             Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
             GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
             NetworkServer.AddPlayerForConnection(conn, player);
             
             keeper.ZeroScores();
-
+            // SpawnObstacles();
             // spawn ball if two players
             if (numPlayers == 2)
             {
@@ -58,28 +96,51 @@ namespace Mirror.AirHockey2077
 
         public void SpawnObstacles()
         {
-            // int numObstacles = Random.Range(1, NUMBER_OF_OBSTACLES + 1);
-            Debug.Log("SpawnObstacles");
-            var obstacle = CreateObstacle();
-            NetworkServer.Spawn(obstacle);
-            Debug.Log("NetworkServer.Spawn(obstacle);");
+            RegenerateRandomPaths();
+
+            obstacles = new List<GameObject>();
+            int numObstacles = Random.Range(1, paths.Count);
+            // int numObstacles = 4;
+            Debug.Log("numObstacles=" + numObstacles);
+            List<int> obstaclesNums = GenerateRandom(numObstacles, NUMBER_OF_OBSTACLES);
+            List<int> pathsNums = GenerateRandomUnique(paths.Count);
+            PrintList(obstaclesNums, "obstaclesNums");
+            PrintList(pathsNums, "pathsNums");
+            for(int i = 0; i < numObstacles; i++)
+            {
+                var obstacle = CreateObstacle(obstaclesNums[i] + 1, pathsNums[i]);
+                obstacles.Add(obstacle);
+                NetworkServer.Spawn(obstacle);
+            }
+
+            // Debug.Log("SpawnObstacles");
+            // var obstacle = CreateObstacle(4, 0);
+            // NetworkServer.Spawn(obstacle);
+            // Debug.Log("NetworkServer.Spawn(obstacle);");
         }
 
-        private GameObject CreateObstacle()
+        private GameObject CreateObstacle(int obstacleNum, int pathNum)
         {
-            GameObject obstacle = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Obstacle4"));
-            Debug.Log("Instantiate");
+            Debug.Log("CreateObstacle" + obstacleNum);
+            GameObject obstacle = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Obstacle" + obstacleNum));
             Obstacle other = (Obstacle) obstacle.GetComponent(typeof(Obstacle));
-            other.sayHello();
-            // other.setStartPosition(new Vector2(-5, 2));
-            Debug.Log("obstacle.sayHello();");
+            other.SetPath(paths[pathNum]);
+            other.SetStartPosition(paths[pathNum][0]);
+            int _speed = Random.Range(10, 20);
+            Debug.Log("_speed=" + _speed);
+            if (Random.value > 0.5f)
+            {
+                int _rotationSpeed = Random.Range(300, 1000);
+                other.SetRotationSpeed(_rotationSpeed);
+                Debug.Log("_rotationSpeed=" + _rotationSpeed);
+            }
             return obstacle;
         }
 
         public void DespawnObstacles()
         {
             // destroy ball
-            if (obstacles != null) {
+            if (obstacles != null) {                
                 foreach(GameObject obstacle in obstacles){
                     NetworkServer.Destroy(obstacle);
                 }
@@ -94,6 +155,70 @@ namespace Mirror.AirHockey2077
 
             // call base functionality (actually destroys the player)
             base.OnServerDisconnect(conn);
+        }
+
+        public static List<int> GenerateRandom(int count, int rangeEnd)
+        {
+            List<int> result = new List<int>();
+            for(int j = 0; j < count; j++)
+            {
+                result.Add(Random.Range(0, rangeEnd));
+            }
+            return result;
+        }
+
+        public static List<int> GenerateRandomUnique(int count)
+        {
+            List<int> result = new List<int>();
+            for(int j = 0; j < count; j++)
+            {
+                result.Add(j);
+            }
+
+            // shuffle the results:
+            int i = result.Count;  
+            while (i > 1)
+            {  
+                i--;  
+                int k = Random.Range(0, i + 1);  
+                int value = result[k];  
+                result[k] = result[i];  
+                result[i] = value;  
+            }  
+            return result;
+        }
+
+        private void RegenerateRandomPaths()
+        {
+            ClearRandomPaths();
+
+            List<Vector2> staticPath = new List<Vector2>();
+            staticPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            paths.Add(staticPath);
+
+            List<Vector2> randomPath = new List<Vector2>();
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            randomPath.Add(new Vector2(Random.Range(-5, 5), Random.Range(-3, 3)));
+            paths.Add(randomPath);
+        }
+
+        private void ClearRandomPaths()
+        {
+            paths.RemoveAt(paths.Count - 1);
+            paths.RemoveAt(paths.Count - 1);
+        }
+
+        private static void PrintList(List<int> myList, string listName)
+        {
+            string result = listName + " contents: ";
+            foreach (var item in myList)
+            {
+                result += item.ToString() + ", ";
+            }
+            Debug.Log(result);
         }
     }
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,53 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror.AirHockey2077
 {
     public class Obstacle : NetworkBehaviour
     {
-        public float speed = 30;
+        public float speed = 10;
+        public float _rotationSpeed = 0;
         public Rigidbody2D rigidbody2d;
-        
-        [SerializeField] private Transform[] waypoints;
+
+        private List<Vector2> path;
         private int waypointIndex = 0;
+        private int direction = 1;
+        private int curPathLen = 0;
 
         public override void OnStartServer()
         {
             base.OnStartServer();
-            transform.position = waypoints[waypointIndex].transform.position;
-            rigidbody2d.position = new Vector2(1, 1);
         }
 
         void FixedUpdate()
         {
             // rigidbody2d.velocity = new Vector2(0, 1) * speed * Time.fixedDeltaTime;
-
             // if (!isServer) return;
-            // rigidbody2d.AddTorque(25 * Time.fixedDeltaTime);
             // rigidbody2d.velocity  = transform.right;
-
+            // rigidbody2d.AddTorque(45 * Time.fixedDeltaTime);
+            
             Move();
         }
 
         private void Move()
         {
-            if (waypointIndex <= waypoints.Length - 1)
+            transform.Rotate(0, 0, _rotationSpeed * Time.fixedDeltaTime);
+            
+            // if (waypointIndex <= path.Count - 1)
+            if (curPathLen <= path.Count)
             {
                 transform.position = Vector2.MoveTowards(transform.position,
-                waypoints[waypointIndex].transform.position,
+                path[waypointIndex],
                 speed * Time.deltaTime);
-                if (transform.position == waypoints[waypointIndex].transform.position)
+                if (transform.position == (Vector3)path[waypointIndex])
                 {
-                    waypointIndex += 1;
+                    // waypointIndex += 1;
+                    NextWaypointIndex();
+                    curPathLen += 1;
                 }
             } else 
             {
-                waypointIndex = 0;
+                // waypointIndex = 0;
+                ChangeDirection();
+                ResetWaypointIndex();
+                ResetCurPathLen();
             }
         }
 
-        public void setStartPosition(Vector2 position)
+        private void ResetWaypointIndex()
         {
-            rigidbody2d.position = position;
+            if (direction < 0) {
+                waypointIndex = path.Count - 1;
+                return;
+            }
+            waypointIndex = 0;
+
+            // Debug.Log("waypointIndex=" + waypointIndex);
+        }
+
+        private void ResetCurPathLen()
+        {
+            if (direction < 0) {
+                curPathLen = 1;
+                return;
+            }
+            curPathLen = 0;
+        }
+
+        private void ChangeDirection()
+        {
+            direction *= -1;
+        }
+
+        private void NextWaypointIndex()
+        {
+            waypointIndex += direction;
+            if (waypointIndex == path.Count)
+            {
+                waypointIndex = 0;
+            }
+            if (waypointIndex < 0)
+            {
+                waypointIndex = path.Count - 1;
+            }
+        }
+
+        private bool CheckEndOfPath()
+        {
+            if (direction < 0)
+            {
+                return waypointIndex >= 0;
+            }
+
+            return waypointIndex <= path.Count;
+        }
+
+        public void SetPath(List<Vector2> newPath)
+        {
+            path = newPath;
+        }
+
+        public void SetStartPosition(Vector2 position)
+        {
+            transform.position = position;
+        }
+
+        public void SetRotationSpeed(float rotationSpeed)
+        {
+            _rotationSpeed = rotationSpeed;
+        }
+
+        public void SetSpeed(float speed)
+        {
+            this.speed = speed;
         }
 
         public void sayHello()
